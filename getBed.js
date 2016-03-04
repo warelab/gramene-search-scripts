b065a511afab37463f337509a10f8bcc9d9ada2d
Andrew Olson <olson@cshl.edu>
2016-03-04 16:21:36 -0500

    use through2 to stream batches

--- getBed.js
+++ getBed.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+var through2 = require('through2');
+var byline = require('byline');
+var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 
 var url = argv.swagger || 'http://data.gramene.org/v50/swagger';
@@ -10,34 +13,53 @@ var batchSize = argv.batchSize || 100;
 global.gramene = {defaultServer: url};
 var gramene = require('gramene-search-client').client.grameneClient;
 if (idFile) {
+  var reader = byline(fs.createReadStream(idFile));
   var ids = [];
-  require('readline').createInterface({
-    input: require('fs').createReadStream(idFile),
-    terminal: false
-  })
-  .on('line', function(line) {
-    ids.push(line);
-  })
-  .on('close', function() {
-    gramene.then(function(client) {
-      console.error('parsed',ids.length,'ids');
-      for(var i=0; i<ids.length; i+=batchSize) {
-        var params = {
-          rows: batchSize,
-          wt: 'bed',
-          bedFeature: feature,
-          bedCombiner: combiner,
-          idList: ids.slice(i, i+batchSize)
-        };
-        if (argv.taxon_id) {
-          params.taxon_id = argv.taxon_id;
-        }
-        client['Data access'].genes(params, function(res) {
-          console.log(res.data);
-        })
+
+  var batcher = function batcher(batchSize) {
+    
+    var transform = function (line, enc, done) {
+      if (ids.length === batchSize) {
+        var batch = ids;
+        this.push(batch);
+        ids = [];
       }
+      ids.push(line);
+      done();
+    };
+    
+    var flush = function(done) {
+      this.push(ids);
+      done();
+    }
+    
+    return through2.obj(transform,flush);
+  };
+
+  var fetcher = through2.obj(function(ids, enc, done) {
+    var that = this;
+    gramene.then(function(client) {
+      var params = {
+        rows: batchSize,
+        wt: 'bed',
+        bedFeature: feature,
+        bedCombiner: combiner,
+        idList: ids
+      };
+      if (argv.taxon_id) {
+        params.taxon_id = argv.taxon_id;
+      }
+      client['Data access'].genes(params, function(res) {
+        that.push(res.data);
+        done();
+      })
     })
-  })
+  });
+
+  reader
+  .pipe(batcher(batchSize))
+  .pipe(fetcher)
+  .pipe(process.stdout);
 }
 else if (argv.taxon_id) {
   gramene.then(function(client) {
